c613df03c28abcc800822f64769977eaf42ea1d6
Moroni <Moroni@192.168.15.16>
2017-01-11 14:09:33 -0200

    login, signup and session working

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['site/contact']],
-            ['label' => 'User', 'url' => ['/user']],
+            ['label' => 'SignUp', 'url' => ['/signup']],
             ['label' => 'Store', 'url' => ['/store']],
             ['label' => 'Assignment', 'url' => ['/user-store-role']],
             Yii::$app->user->isGuest ? (
